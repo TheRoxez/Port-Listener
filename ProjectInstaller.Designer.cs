b62c16b3f830b9269dae174f2c17cc49b3d057ad
Muhammet Ali Bozkurt <128151296+TheRoxez@users.noreply.github.com>
2023-06-08 13:36:28 +0300

    Add files via upload

---
+++ ProjectInstaller.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace PLMSQL
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Bileşen Tasarımcısı üretimi kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Belirli portu dinleyip gelen veriyi veritabanına yazar.";
+            this.serviceInstaller1.DisplayName = "PLMSQL";
+            this.serviceInstaller1.ServiceName = "Port Listener MySQL";
+            this.serviceInstaller1.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceInstaller1_AfterInstall);
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using MySql.Data.MySqlClient;
+
+namespace PLMSQL
+{
+    public partial class Service1 : ServiceBase
+    {   
+        private TcpListener listener;
+        protected override void OnStart(string[] args)
+        {
+            LogWrite("Servis Başlatıldı. " + DateTime.Now);
+            listener = new TcpListener(IPAddress.Any, 5555);
+            listener.Start();
+            
+            listener.BeginAcceptTcpClient(HandleTcpClientConnected, null);
+            LogWrite("Bağlantı Alındı. " + DateTime.Now);
+        }
+
+        protected override void OnStop()
+        {
+            listener.Stop();
+            LogWrite("Servis Sonlandırıldı. " + DateTime.Now);
+        }
+
+        private void HandleTcpClientConnected(IAsyncResult result)
+        {
+            TcpClient client = listener.EndAcceptTcpClient(result);
+            listener.BeginAcceptTcpClient(HandleTcpClientConnected, null);
+
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            string receivedData = "";
+
+            while (true)
+            {
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                receivedData = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                string connectionString = "server=localhost;user=root;database=test;password=6660;";
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO new_table (nm) VALUES (@nm)";
+
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@nm", receivedData);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        public void LogWrite(string message)
+        {
+            string dyol = AppDomain.CurrentDomain.BaseDirectory + "/Logs";
+            if (!Directory.Exists(dyol))
+            {
+                Directory.CreateDirectory(dyol);
+            }
+            string txtyol = AppDomain.CurrentDomain.BaseDirectory + "/Logs/servis.txt";
+            if (!File.Exists(txtyol))
+            {
+                using (StreamWriter sv = File.CreateText(txtyol))
+                {
+                    sv.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sv = File.AppendText(txtyol))
+                {
+                    sv.WriteLine(message);
+                }
+            }
+        }     
+    }
+}
